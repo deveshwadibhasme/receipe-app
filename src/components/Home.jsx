35ce6043604c515da992ebde8ff438ba6557a972
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-18 23:51:50 +0530

    all set

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ const Home = ({ handleInput, input, meals, placeHolder }) => {
     return (
         <section
             className='min-h-screen w-screen bgImage bg-no-repeat bg-cover bg-[70%_40%] md:bg-[0_0]'>
-
             <div className='max-w-xl min-h-64 relative left-1/2 -translate-x-1/2 translate-y-1/2 flex items-center flex-col'>
                 <h1 className='text-4xl mb-2 font-bold text-yellow-200 text-border'>Find Your Receipe</h1>
                 <input
@@ -17,7 +16,7 @@ const Home = ({ handleInput, input, meals, placeHolder }) => {
                 />
             </div>
             {meals && <div key={crypto.randomUUID()} className='grid grid-cols-2 md:grid-cols-4 mx-auto max-w-5xl overflow-y-scroll suggestion gap-3 md:gap-6 h-96 md:h-80'>
-                {meals.length === 0 || input === '' ? 'Nothing Here' : meals.map((meal) => (
+                {meals.length === 0 || input === '' ? <h1 className='mx-auto font-bold'>Nothing Here</h1> : meals.map((meal) => (
                     <MealSuggestion key={meal.idMeal} meal={meal} />
                 ))}
             </div>}
--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -3,8 +3,11 @@ import React, { useState } from 'react'
 const Favourite = () => {
 
   return (
-    <div className='min-h-screen w-screen bgImage bg-no-repeat bg-cover bg-[70%_40%] md:bg-[0_0]'>
-      Favourite
+    <div style={{ paddingTop: 200 }} className='min-h-screen w-screen bgImage bg-no-repeat bg-cover bg-[70%_40%] md:bg-[0_0]'>
+      <h1 className='text-white font-bold text-border'>
+        I will implements this tab... when I learned the concept of Context API in React.
+        I will definetely share this with community.
+      </h1>
     </div>
   )
 }
