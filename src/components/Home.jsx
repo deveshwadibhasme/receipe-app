afce83901cd30fdd431a5e51b9903f7041d10b1c
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-18 18:01:00 +0530

    all set

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = ({ handleInput, input, meals }) => {
     return (
         <section
             className='min-h-screen w-screen bgImage bg-no-repeat bg-cover bg-[70%_40%] md:bg-[0_0]'>
+               
             <div className='max-w-xl min-h-64 relative left-1/2 -translate-x-1/2 translate-y-1/2 flex items-center flex-col'>
                 <h1 className='text-4xl mb-2 font-bold text-yellow-200 text-border'>Find Your Receipe</h1>
                 <input
@@ -15,6 +16,7 @@ const Home = ({ handleInput, input, meals }) => {
                     onChange={handleInput}
                 />
             </div>
+            <marquee className='text-white' behavior="smooth" direction="right">In Development...!!  &copy; Devesh Wadibhasme</marquee>
             {meals && <div key={crypto.randomUUID()} className='grid grid-cols-2 md:grid-cols-4 mx-auto max-w-5xl overflow-y-scroll gap-3 md:gap-6 h-96 md:h-80'>
                 {meals.length === 0 || input === '' ? 'Nothing Here' : meals.map((meal) => (
                     <MealSuggestion key={meal.idMeal} meal={meal} />
